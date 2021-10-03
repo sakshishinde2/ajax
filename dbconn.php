136dbc5aee9cc3980a4300e7c9c02959b729c05a
Sakshi <sakshishinde808@gmail.com>
2021-10-03 16:49:32 +0530

    working search box

--- dbconn.php
+++ dbconn.php
@@ -1,25 +1,73 @@
 <?php
+//fetch.php
+if(isset($_POST["query"]))
+{
+ //$connect = mysqli_connect("localhost", "root", "", "demo");
+ $myPDO = new PDO('sqlite:demo_sqlite.db');
 
-$myPDO = new PDO('sqlite:demo_sqlite.db');
+ //$request = mysqli_real_escape_string($connect, $_POST["query"]);
+ $request =$_POST["query"];
 
-if (!$myPDO) {
-    echo "Connection failed!";
-}else{
-    //echo "connection successful";
+ $query = "
+  SELECT * FROM clients 
+  WHERE username LIKE '%".$request."%' 
+  OR ntrip_id LIKE '%".$request."%' 
+ ";
+ //$result = mysqli_query($connect, $query);
+ $results=$myPDO->query($query);
 
+ $data =array();
+ $html = '';
+ $html .= '
+  <table class="table table-bordered table-striped">
+   <tr>
+    <th>Name</th>
+    <th>NTRIP Id</th>
+    <th>GPS status</th>
+   </tr>
+  ';
+ //if(mysqli_num_rows($result) > 0)
+ if($results !== FALSE)
+ {
+    $data1=$results->fetchAll(); 
+    $row_count=count($data1);
+    if($row_count > 0){
+  //while($row = mysqli_fetch_array($result))
+  foreach($data1 as $row)
+  {
+   $data[] = $row["username"];
+   $data[] = $row["ntrip_id"];
+   $data[] = $row["GPS_status"];
+   $html .= '
+   <tr>
+    <td>'.$row["username"].'</td>
+    <td>'.$row["ntrip_id"].'</td>
+    <td>'.$row["GPS_status"].'</td>
+   </tr>
+   ';
+  }
+  }
+ }
+ else
+ {
+  $data = 'No Data Found';
+  $html .= '
+   <tr>
+    <td colspan="3">No Data Found</td>
+   </tr>
+   ';
+ }
+ $html .= '</table>';
+ if(isset($_POST['typehead_search']))
+ {
+  echo $html;
+ }
+ else
+ {
+  //$data = array_unique($data);
+  echo json_encode($data);
+  
+ }
 }
-
-//*get data in json format
-// $results= $myPDO->query("SELECT * FROM clients");  
-// if ($results !== null) {
-//create an array
-//     $emparray = array();
-//     while($row = $results->fetch(PDO::FETCH_ASSOC)){
-
-//         $emparray[] = $row;
-//     }
-//     echo json_encode($emparray);
-// }  
-           
 
 ?>
